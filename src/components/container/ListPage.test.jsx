ffb31a2fab9c5dc0dd4eb6c5d7d829ae39a4c234
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-19 09:41:08 -0700

    create List component

--- src/components/container/ListPage.test.jsx
+++ src/components/container/ListPage.test.jsx
@@ -3,19 +3,27 @@ import { render, screen } from '@testing-library/react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import starTrekData from '../../TestData/startrek.json'
+import ListPage from './ListPage'
 describe('ListPage component', () => {
   const server = setupServer(
     rest.get('https://trek-dex.herokuapp.com/api/v1/characters', (req, res, ctx) => {
       res(ctx.json(starTrekData))
     })
   )
+  
+  const testProps = {
 
+  }
+  
   beforeAll(() => server.listen())
   afterAll(() => server.close())
 
   it('renders a list of Star Trek characters to the screen on load', async () => {
-    screen.getByText('Loading')
+    render(<ListPage {...testProps} />)
+
+    screen.getByText('Loading', {exact: false})
     const charList = await screen.findByRole('list', {name: 'characters'})
+    screen.getByText('Scotty', {exact: false})
     expect(charList).not.toBeEmptyDOMElement()
   })
 })
--- src/components/display/List.jsx
+++ src/components/display/List.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function List(props) {
+function List({name, origin, image}) {
   return (
     <div>
-      
+      <h1>{name}</h1>
+      <img src={image} alt={name} />
+      <p>{origin}</p>
     </div>
   )
 }
 
 List.propTypes = {
-
+  name: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
 }
 
 export default List
