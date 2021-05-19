706aadf1c77cda5a1c466fd12bcf1eb7d32a629e
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-19 09:43:29 -0700

    create detail page

--- src/components/display/Detail.jsx
+++ src/components/display/Detail.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Detail(props) {
+function Detail({name, affiliation, origin, race, image}) {
   return (
     <div>
-      
+      <h1>{name}</h1>
+      <img src={image} alt={name} />
+      <p>{affiliation}</p>
+      <p>{race}</p>
+      <p>{origin}</p>
     </div>
   )
 }
 
 Detail.propTypes = {
-
+  name: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  affiliation: PropTypes.string.isRequired,
+  race: PropTypes.string.isRequired,
 }
 
 export default Detail
--- src/components/display/List.jsx
+++ src/components/display/List.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function List({name, origin, image}) {
+function List({name, image}) {
   return (
     <div>
       <h1>{name}</h1>
       <img src={image} alt={name} />
-      <p>{origin}</p>
     </div>
   )
 }
 
 List.propTypes = {
   name: PropTypes.string.isRequired,
-  origin: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
 }
 
